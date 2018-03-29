dc7d899cb18b05347c07a4a5483afe225f7566d2
Henryk Krzysztof Blasinski <hblasins@stanford.edu>
2018-03-28 22:15:22 -0700

    Updated a demo rendering file and the utility function it used.

--- Code/Rendering/Utilities/mergeMetadata.m
+++ Code/Rendering/Utilities/mergeMetadata.m
@@ -1,34 +1,37 @@
 function [ classMap, instanceMap ] = mergeMetadata( fileName, labelMap, varargin )
 
-% [ simplifiedMap ] = mergeMetadata( fileName, classesToMerge, ... )
+% [ classMap, instanceMap ] = mergeMetadata( fileName, labelMap, ... )
 %
-% This function accepts a path to a .mat file that represents the output
+% This function accepts a path to a .dat file that represents the output
 % from a 'mesh' or 'material' metadata PBRT renderer. It outputs a
 % simplified map with fewer meshes or materials than in the original file.
-% The classes are groupped accoridng the presence of common strings in
-% their class descriptions in the accompanying .txt file. For example if
-% classesToMerge = {'car','city'}, then all class names contaning the
-% substring 'car' will be assigned one class and all class names containing
-% the substring 'city' the other. 
+% The classes are groupped accoridng the field 'class' in labelMap. For example if
+% labelMap(1).class = 'car' and labelMap(2).class = 'city', then all class 
+% names contaning the substring 'car' will be assigned one class and all 
+% class names containing the substring 'city' the other. 
 %
-% It is assumed tha strings in classesToMerge are mutually exclusive, i.e.
-% there is no class that could contain two or more strings from
-% classesToMerge cell array.
+% The function returns a classMap, where each pixel is labeled according to
+% the class it belongs to, and an instanceMap, where each pixel represents
+% object instance it belongs to. These two are necessary to automatically
+% draw bounding boxes.
 %
-% Copyright, Henryk Blasinski 2017
+% Copyright, Henryk Blasinski 2018
 
 
 p = inputParser;
 p.addRequired('fileName');
-p.addRequired('classesToMerge',@isstruct);
+p.addRequired('labelMap');
 p.addOptional('mode','mesh');
 
 p.parse(fileName,labelMap,varargin{:});
 mode = p.Results.mode;
 
-load(fileName);
+% load(fileName);
+% map = uint32(img/radiometricScaleFactor);
+
+multispectralImage = piReadDAT(fileName, 'maxPlanes', 31);
 img = multispectralImage(:,:,1);
-map = uint32(img/radiometricScaleFactor);
+map = uint32(img);
 
 [path, fName] = fileparts(fileName);
 switch mode
@@ -38,18 +41,22 @@ switch mode
         textFile = fullfile(path,sprintf('%s_texture.txt',fName));
 end
 
-fid = fopen(textFile);
+classMap = zeros(size(map));
+instanceMap = zeros(size(map));
+
+fid = fopen(textFile,'r');
+if fid == -1
+    return;
+end
 mappings = textscan(fid,'%u %s');
 fclose(fid);
 nSimplified = length(labelMap);
    
-classMap = zeros(size(map));
-instanceMap = zeros(size(map));
-instanceId=1;
+
 for i=1:nSimplified
 
     % Find all the meshes that contain the class in their name
-    loc = cellfun(@(x) isempty(x)==false, (strfind(lower(mappings{2}),lower(labelMap(i).name))));
+    loc = cellfun(@(x) isempty(x)==false, (strfind(lower(mappings{2}),sprintf('_%s_',lower(labelMap(i).name)))));
     classMap(ismember(map,mappings{1}(loc))) = labelMap(i).id;
     
     meshNames = mappings{2}(loc);
@@ -58,26 +65,20 @@ for i=1:nSimplified
     % Find all instances belonging to the category
     posA = strfind(lower(meshNames),lower('_inst_'));
     
-    if any(cellfun(@isempty,posA))
-        % We don't differentiate between instances
-        instanceMap(ismember(map,meshIds)) = instanceId;
-        instanceId = instanceId + 1;
-    else
-        % but if we find the keyword _inst_ then we have different
-        % instances
-        posB = cellfun(@(x,y) strfind(y(x(1)+length('_inst_'):end),'_'),posA,meshNames,'UniformOutput',false);
-        posB = cellfun(@(x) x(1),posB,'UniformOutput',false);
+    
+    % but if we find the keyword _inst_ then we have different
+    % instances
+    posB = cellfun(@(x,y) strfind(y(x(1)+length('_inst_'):end),'_'),posA,meshNames,'UniformOutput',false);
+    posB = cellfun(@(x) x(1),posB,'UniformOutput',false);
+    
+    instances = cellfun(@(x,y,z) str2double(z(x+length('_inst_'):(x+length('_inst_')+y-2))),posA,posB,meshNames);
+    instances = unique(instances);
+    nInstances = length(instances);
+    
+    for j=1:nInstances
+        loc = cellfun(@(x) isempty(strfind(lower(x),sprintf('_inst_%i_',instances(j))))==false,meshNames);
         
-        instances = cellfun(@(x,y,z) str2double(z(x+length('_inst_'):(x+length('_inst_')+y-2))),posA,posB,meshNames);
-        instances = unique(instances);
-        nInstances = length(instances);
-        
-        for j=1:nInstances
-            loc = cellfun(@(x) isempty(strfind(lower(x),sprintf('_inst_%i_',instances(j))))==false,meshNames);
-            
-            instanceMap(ismember(map,meshIds(loc))) = instanceId;
-            instanceId = instanceId + 1;
-        end
+        instanceMap(ismember(map,meshIds(loc))) = instances(j);
     end
     
 end
