96df45947e16ca589b42b5ff345b9f70b01f95f3
Brian Wandell <wandell@stanford.edu>
2018-04-01 22:55:08 -0700

    Updated for ZL to test

--- GM2018/iaGMsensor.m
+++ GM2018/iaGMsensor.m
@@ -1,37 +1,71 @@
 %% Script to illustrate city with different sensors
 %
+%  At this point we have a default, foggy car scene.  But we will update
+%  that with another OI.
 %
-% ZL/BW SCIEN
+%  The current sensors are the 6 micron MT9V024 line.
+%
+%  We have part of the AR0132 line implemented and we could do more on that.
+%  That is a 3.75 micron pixel
+%
+% Copyright ZL/BW SCIEN Stanford, April, 1, 2018
 
 %% Read in the oi from somehwere
-% In this example I just ran
-%
+
+% If you don't have the AWS access below, you can create an OI this way
 %{
-s_piReadRenderLens
+ scene = sceneCreate('sweep frequency');
+ scene = sceneSet(scene,'fov',30);
+ oi = oiCreate;
+ oi = oiCompute(oi,scene);
 %}
-% which produced a crummy oi.
 
-%% Create a monochrome sensor
+%%
+ieInit
 
-sensor = sensorCreate('monochrome');
+%% Download the car in fog example sensor irradiance
+rdt = RdtClient('isetbio');
+rdt.crp('/resources/oi');
+oiCarFog = rdt.readArtifact('oiCarFog','type','mat');
+oi = oiCarFog.oi;
+ieAddObject(oi); oiWindow;
+
+%% Calculate a sensor response
+
+% This is the ON RGB sensor
+load('MT9V024SensorRGB','sensor');
+oi = oiSet(oi,'fov',40);
+sensor = sensorSetSizeToFOV(sensor,oiGet(oi,'fov'));
+sensor = sensorCompute(sensor,oi);
+
+ieAddObject(sensor); sensorWindow; truesize
+
+%% Load a monochrome sensor
+
+load('MT9V024SensorMono','sensor');
 
 % Set its field of view to match the OI field of view.
 % This changes the number of rows and columns.
 sensor = sensorSetSizeToFOV(sensor,oiGet(oi,'fov'));
-sensor = sensorSet(sensor,'exposure duration',0.002);  % 2 ms exposure
-
+sensor = sensorSet(sensor,'auto exposure',true);  % 2 ms exposure
 sensor = sensorCompute(sensor,oi);
 
-ieAddObject(sensor);
-sensorWindow;
+ieAddObject(sensor); sensorWindow;
 
 % This sets it up to display reasonably for the high dynamic range.
 sensor = sensorSet(sensor,'gamma',0.7);
 
 %% An RCCC camera design - to be fixed up by BW
 
-foo = load('MT9V024_Mono');
+load('MT9V024SensorRCCC','sensor');
 
-%% Change the pixel size like this
-
+sensor = sensorSetSizeToFOV(sensor,oiGet(oi,'fov'));
+sensor = sensorSet(sensor,'auto exposure',true);  % 2 ms exposure
 sensor = sensorCompute(sensor,oi);
+
+ieAddObject(sensor); sensorWindow;
+
+% This sets it up to display reasonably for the high dynamic range.
+sensor = sensorSet(sensor,'gamma',0.7);
+
+%%
