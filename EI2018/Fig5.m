b621aef0b930418f31f9eaba30d0681c475fabc5
Henryk Błasiński <hblasins@stanford.edu>
2018-04-30 21:40:13 -0700

    Modified figure apperance.

--- EI2018/Fig5.m
+++ EI2018/Fig5.m
@@ -15,62 +15,42 @@ end
 
 %% SSD-MobileNet
 
-data = [0.59 0.54 0.65;
-        0.54 0.25 0.48]';
-
-ref = 0.61;
-    
-legendEntries = {'optimal',...
-                 'sRGB transfer',...
-                 'sRGB'};
+data = [0.54 0.65 0.61];
+  
 
 figure;
 hold on; grid on; box on;
-bar(data);
-plot(-10:10,ref*ones(21,1),'--r','LineWidth',lw);
-set(gca,'FontName','Arial');
+br = bar(data,'facecolor','black','barwidth',0.5);
+set(gca,'FontName','Helvetica');
 set(gca,'FontSize',fs-2);
 ylim([0 1]);
 xlim([0.5 3.5]);
-lg = legend(legendEntries,'FontSize',fs-2,'orientation','horizontal');
-set(lg,'Position',[0.43 0.80 0.1448 0.0817]);
 ylabel('mAP @ IOU 0.5','FontSize',fs);
 set(gca,'XTick',1:3);
-set(gca,'XTickLabel',{'irradiance','RAW','linear'});
-xlabel('Data representation','FontSize',fs);
+set(gca,'XTickLabel',{'raw','linear','sRGB'});
 set(gcf,'PaperUnits','centimeters');
 set(gcf,'PaperPosition',pos);
 
-fName = fullfile(destPath,'representation_SSD.png');
-print('-dpng',fName);
+fName = fullfile(destPath,'representation_SSD.eps');
+print('-depsc',fName);
 
 %% RFCN
 
-data = [0.78 0.71 0.75;
-        0.74 0.55 0.71]';
+data = [0.71 0.75 0.76]';
 
-ref = 0.76;
-    
-legendEntries = {'optimal',...
-                 'sRGB transfer',...
-                 'sRGB'};
 
 figure;
 hold on; grid on; box on;
-bar(data);
-plot(-10:10,ref*ones(21,1),'--r','LineWidth',lw);
-set(gca,'FontName','Arial');
+br = bar(data,'facecolor','black','barwidth',0.5);
+set(gca,'FontName','Helvetica');
 set(gca,'FontSize',fs-2);
 ylim([0 1]);
 xlim([0.5 3.5]);
-lg = legend(legendEntries,'FontSize',fs-2,'orientation','horizontal');
-set(lg,'Position',[0.43 0.80 0.1448 0.0817]);
 ylabel('mAP @ IOU 0.5','FontSize',fs);
 set(gca,'XTick',1:3);
-set(gca,'XTickLabel',{'irradiance','RAW','linear'});
-xlabel('Data representation','FontSize',fs);
+set(gca,'XTickLabel',{'raw','linear','sRGB'});
 set(gcf,'PaperUnits','centimeters');
 set(gcf,'PaperPosition',pos);
 
-fName = fullfile(destPath,'representation_RFCN.png');
-print('-dpng',fName);
+fName = fullfile(destPath,'representation_RFCN.eps');
+print('-depsc',fName);
--- EI2018/Fig7.m
+++ EI2018/Fig7.m
@@ -4,11 +4,8 @@ clc;
 
 fs = 8;
 lw = 1;
-ms = 2;
-pos = [1 1 10 4];
-cmap = jet(4);
-cmap = cmap([1 2 4],:);
-markers = {'-x','-o','-d','-*'};
+ms = 10;
+pos = [1 1 5 7];
 
 destPath = fullfile('~','Desktop','Figures','ML');
 if ~exist(destPath,'dir');
@@ -17,56 +14,60 @@ end
 
 %% SSD
 
-data = [0.263 0.417 0.519 0.576 0.614 0.585 0.417 0.203 0.117;
-        0.549 0.617 0.628 0.614 0.614 0.567 0.473 0.359 0.166];          
-    
-legendEntries = {'0EV',...
-                 'xEV'};
+ev = -4:4;
+data = [0.263 0.417 0.519 0.576 0.614 0.585 0.417 0.203 0.117];          
+mix = [0.367 0.426 0.448 0.449 0.458 0.460 0.424 0.343 0.223];
+bound = [0.549 0.617 0.628 0.614 0.614 0.567 0.473 0.359 0.166];
+
+
 
 figure;
-hold on; grid on; box on;
-for i=1:size(data,1)
-    plot(-4:4,data(i,:),markers{i},'lineWidth',lw,'color',cmap(i,:),'markerSize',ms);
-end
-set(gca,'FontName','Arial');
+box on;
+
+fill([ev fliplr(ev)],[bound ones(1,9)],[0.9 0.9 0.9],'EdgeColor','none');
+grid on; hold on;
+
+plot(ev,data,'marker','.','lineWidth',lw,'color','black','markerSize',ms);
+plot(ev,mix,'--','marker','.','lineWidth',lw,'color','black','markerSize',ms);
+
+set(gca,'FontName','Helvetica');
 set(gca,'FontSize',fs-2);
 ylim([0 1]);
-lg = legend(legendEntries,'FontSize',fs-2,'orientation','horizontal');
-set(lg,'Position',[0.40 0.23 0.1448 0.0817]);
-ylabel('mAP @ IOU 0.5','FontSize',fs);
+ylabel('mAP @ IoU 0.5','FontSize',fs);
 xlabel('Exposure bias, EV','FontSize',fs);
 
 set(gcf,'PaperUnits','centimeters');
 set(gcf,'PaperPosition',pos);
 
-fName = fullfile(destPath,'exposure-SSD.png');
-print('-dpng',fName);
+fName = fullfile(destPath,'exposure-SSD.eps');
+print('-depsc',fName);
 
 %% RFCN
 
-data = [0.59 0.70 0.75 0.76 0.77 0.73 0.60 0.40 0.22;
-        0.75 0.76 0.76 0.78 0.77 0.75 0.72 0.64 0.56];
-    
-legendEntries = {'0EV',...
-                 'xEV'};
+data = [0.59 0.70 0.75 0.76 0.77 0.73 0.60 0.40 0.22];
+bound = [0.75 0.76 0.76 0.78 0.77 0.75 0.72 0.64 0.56];
+mix = [0.67 0.70 0.71 0.71 0.70 0.69 0.64 0.58 0.44];    
+
 
 figure;
-hold on; grid on; box on;
-for i=1:size(data,1)
-    plot(-4:4,data(i,:),markers{i},'lineWidth',lw,'color',cmap(i,:),'markerSize',ms);
-end
-set(gca,'FontName','Arial');
+box on;
+fill([ev fliplr(ev)],[bound ones(1,9)],[0.9 0.9 0.9],'EdgeColor','none');
+grid on; hold on;
+
+plot(ev,data,'marker','.','lineWidth',lw,'color','black','markerSize',ms);
+plot(ev,mix,'--','marker','.','lineWidth',lw,'color','black','markerSize',ms);
+    
+    
+set(gca,'FontName','Helvetica');
 set(gca,'FontSize',fs-2);
 ylim([0 1]);
-lg = legend(legendEntries,'FontSize',fs-2,'orientation','horizontal','FontName','Arial');
-set(lg,'Position',[0.40 0.23 0.1448 0.0817]);
-ylabel('mAP @ IOU 0.5','FontSize',fs);
+ylabel('mAP @ IoU 0.5','FontSize',fs);
 xlabel('Exposure bias, EV','FontSize',fs);
 
 set(gcf,'PaperUnits','centimeters');
 set(gcf,'PaperPosition',pos);
 
-fName = fullfile(destPath,'exposure-RFCN.png');
-print('-dpng',fName);
+fName = fullfile(destPath,'exposure-RFCN.eps');
+print('-depsc',fName);
 
 
