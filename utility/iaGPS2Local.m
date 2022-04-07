7d818a2dbf4e1af582bf930701474786ab2c85da
Zhenyi Liu <zhenyi27@stanford.edu>
2022-04-07 20:34:25 +0800

    bug fixed

--- utility/iaGPS2Local.m
+++ utility/iaGPS2Local.m
@@ -1,15 +1,20 @@
-
-
-
-function [e, n, u] = iaGPS2Local(lat, lon, alt, origin)
-%%
+function [east, north, up] = iaGPS2Local(lat, lon, alt, origin)
+%% ISET implementation of latlon2local
+% Convert from geographic to local Cartesian coordinates
+%   transforms point locations from (lat, lon, alt) in degrees and
+%   meters to local Cartesian coordinates (xEast, yNorth, zUp) in meters.
+%   The local coordinate system is anchored at origin specified as
+%   [latOrigin, lonOrigin, altOrigin] vector. Local x, y, z coordinates 
+%   are lined up with east, north and up directions respectively.
+%   alt and altOrigin are altitudes as returned by a typical GPS sensor.
 %
+% Zhenyi, 2022
 %{
-lon = 0;
-lat = 0.0017966305679426294;
+lon = 0.00001;
+lat = 0.0019;
 alt = 0;
 origin = [0,0,0]; % in LLA degree
-[e, n, u] = piGPS2Local(lat, lon, alt, origin)
+[e, n, u] = iaGPS2Local(lat, lon, alt, origin)
 % matlab implementation
 [e_m, n_m, u_m] = latlon2local(lat, lon, alt, origin)
 %}
@@ -19,27 +24,31 @@ ecefPOI = piGPS2ECEF(origin(1),origin(2),origin(3));
 ecefUser = piGPS2ECEF(lat,lon, alt);
 
 
-vector(1)=ecefUser(1)-ecefPOI(1);
-vector(2)=ecefUser(2)-ecefPOI(2);
-vector(3)=ecefUser(3)-ecefPOI(3);
+delta_X=ecefUser(1)-ecefPOI(1);
+delta_Y=ecefUser(2)-ecefPOI(2);
+delta_Z=ecefUser(3)-ecefPOI(3);
 
-e = vector(1)*(-sind(lon)) + vector(1)*cosd(lon);
-n = vector(1)*(-sind(lat))*cosd(lon) + vector(2)*(-sind(lat))*(sind(lon)) + vector(3)*cosd(lat);
-u = vector(1)*(cosd(lat))*cosd(lon) + vector(2)*(cosd(lat))*(sind(lon)) + vector(3)*sind(lat);
+phi   =origin(1);
+lamda =origin(2);
+
+ECEF2ENU_matrix = [-sind(lamda),cosd(lamda),0;...
+            -sind(phi)*cosd(lamda),-sind(phi)*sind(lamda),cosd(phi);...
+            cosd(phi)*cosd(lamda),cosd(phi)*sind(lamda),sind(phi)];
+
+result = ECEF2ENU_matrix*[delta_X;delta_Y;delta_Z];
+
+east  = result(1); 
+north = result(2); 
+up    = result(3);
 end
 
-
-
 function ecef = piGPS2ECEF(lat, lon, alt)
-a = 6378.1e3;
-b = 6356.8e3;
+a = 6378.137e3;
+b = 6356.752314245e3;
 
 e = 1-b^2/a^2;
 
-N = a/sqrt(1 - e*(sind(lat)^2));
-
-cosLatRad = cosd(lat);
-cosLongiRad = cosd(lon);
+N = a/sqrt(1 - e*(sind(lat))^2);
 
 % LLA to ECEF
 ecef(1)= (N + alt)*cosd(lat)*cosd(lon);
