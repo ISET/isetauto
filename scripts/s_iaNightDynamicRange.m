ca23021bd5747535848e8dabf6718091a04452f3
Brian Wandell <wandell@stanford.edu>
2022-11-07 16:44:15 -0800

    Update s_iaNightDynamicRange.m

--- scripts/s_iaNightDynamicRange.m
+++ scripts/s_iaNightDynamicRange.m
@@ -16,27 +16,22 @@ chdir(ddir)
 %% Camera was using speed of 437 for some reason
 % Maybe we should set the speed to 55, which is unity gain.
 
-fname = 'IMG_20221106_180748';
-fname = [fname,'_4.dng'];
+% fname = 'IMG_20221106_180748';
+% roinames = {'sky','lamp','road'};
 
-exist(fname,'file')
+basename = 'IMG_20221106_175143';
+roinames = {'stop','sky','road'};
 
+fname = sprintf('%s_%0d.dng',basename,4);
 [sensor,info] = sensorDNGRead(fname);
-sensorWindow(sensor);
 
 roi = cell(3,1);
-names = {'sky','lamp','road'};
 for ii=1:3
     roi{ii} = ieROISelect(sensor);
 end
 
-sensor = sensorSet(sensor,'roi',round(roi{1}));
-dv = sensorGet(sensor,'roi dv');
-mean(dv,'omitnan')
 
 %%
-basename = 'IMG_20221106_180748';
-
 ieNewGraphWin;
 val = zeros(5,3);
 t = zeros(5,1);
