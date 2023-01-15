e9f16813e6398c480176cff85437a501a82de759
David Cardinal <github@cardinalphoto.com>
2023-01-15 07:21:20 -0800

    add function version of scene creation code

--- utility/makeScenesFromRenders.m
+++ utility/makeScenesFromRenders.m
@@ -1,5 +1,8 @@
-function foo = makeScenesFromRenders(renders, varargin)
+function makeScenesFromRenders(varargin)
 %MAKESCENESFROMRENDERS Create ISET Scene Objects from PBRT .exr files
+
+% TBD -- Only partially parameterized. Needs params to select folder
+% with .exr files to be assembled -- DJC
 
 % This function reads rendered EXR files from PBRT, returns ISET scenes/OIs,
 % then saves them in outputFolder in .mat format.
@@ -20,17 +23,18 @@ function foo = makeScenesFromRenders(renders, varargin)
 % D. Cardinal, Stanford University, 2023
 %
 
-p = inputparser();
+p = inputParser();
 
 %% Set dataset parameters for this run
-% These appear constant so define them at the top
+% These are constant for each run so define them at the top
+addParameter(p, 'experimentname', sprintf('%s',datetime('now','Format','yy-MM-dd-HH-mm')), @ischar);
 addParameter(p, 'meanluminance', 5) ; % Default is currently night time
 
 % Light source weightings, Defaults are what was used for Ford Project
-addParameter(p, 'skyL_wt', 10);
-addParameter(p, 'headL_wt', 1);
-addParameter(p,'otherL_wt', 1);
-addParameter(p,'streetL_wt',0.5);
+addParameter(p, 'skyl_wt', 10);
+addParameter(p, 'headl_wt', 1);
+addParameter(p, 'otherl_wt', 1);
+addParameter(p, 'streetl_wt',0.5);
 
 % We can also add flare simulation via the Optics
 addParameter(p, 'flare', 1);
@@ -39,12 +43,20 @@ addParameter(p, 'flare', 1);
 varargin = ieParamFormat(varargin);
 p.parse(varargin{:});
 
+% Place lighting parameters in the params struct used with Ford
+params.meanLuminance = p.Results.meanluminance;
+params.skyL_wt = p.Results.skyl_wt;
+params.headL_wt = p.Results.headl_wt;
+params.otherL_wt = p.Results.otherl_wt;
+params.streetL_wt = p.Results.streetl_wt;
+params.flare = p.Results.flare;
+
 % Set initial locations -- Hard-coded for now!
 if ispc
     % a WebDAV mount
     datasetRootPath = 'V:\data\iset\isetauto';
     % for speed of saving use a local SSD
-    datasetCachePath = 'B:\data\iset\isetauto';
+    datasetCachePath = 'C:\data\iset\isetauto';
     % pick a folder that's downloaded
     renderFolders = [6];
     maxScenes = -2; % for testing
@@ -80,23 +92,34 @@ for rr = renderFolders(1):renderFolders(end)
 
     % Select a folder to contain all processed scenes
     % using these parameters
-    experimentFolderName = 'skymap_scale10';
+    experimentFolderName = ['scenes-' p.Results.experimentname];
 
-    % Then group by the ##
+    % Then group by the ## and create folders
     sceneOutputFolder = sprintf('nighttime_%03d',rr);
-    outputFolder = fullfile(datasetCachePath,...
-        'dataset',experimentFolderName,sceneOutputFolder);
 
+    datasetCacheFolder = fullfile(datasetCachePath,...
+        'dataset');
+    if ~exist(datasetCacheFolder, 'dir'), mkdir(datasetCacheFolder);end
+
+    experimentFolder = fullfile(datasetCacheFolder,experimentFolderName);
+    if ~exist(experimentFolder, 'dir'), mkdir(experimentFolder);end
+    
+    outputFolder = fullfile(experimentFolder,sceneOutputFolder);
     if ~exist(outputFolder, 'dir'), mkdir(outputFolder);end
 
 
     scene = []; % initialize to keep parfor happy
     photons = []; % keep parfor happy
 
+    % Visual wavelengths by default
+    wavelengths = 400:10:700;
+    numBands = numel(wavelengths);
+
+   
     %% Generate dataset
     % USE PARFOR for performance, for for debugging...
     % Except we get an error call save for parsave from this loop?
-    for ii = 1:numel(sceneNames)
+    parfor ii = 1:2 % debug with 2 numel(sceneNames)
         %for ii = 1:numel(sceneNames)
 
         thisSName = erase(sceneNames(ii).name,'_instanceID.exr');
@@ -107,49 +130,35 @@ for rr = renderFolders(1):renderFolders(end)
 
         % Combine pre-rendered .exr files (which each have one type of
         % light source) into the desired combination.
-        if p.Results.skyL_wt > 0
-            %         scene_lg{1} = piEXR2ISET(fullfile(DatasetFolder, 'renderings',[thisSName, '_skymap.exr']),'meanluminance',0);
-
+        if params.skyL_wt > 0
             sky_energy = piReadEXR(fullfile(datasetFolder, [thisSName, '_skymap.exr']), 'data type','radiance');
-
-            %         sky_energy = sky_energy/thisSkyLevel * skyMLum;
-            %         weights(1) = params.skyL_wt;
         else
-            sky_energy = zeros(1080, 1920, 31);
+            sky_energy = zeros(1080, 1920, numBands);
         end
 
         % Auto Headlights
-        if p.Results.headL_wt > 0
-            %         scene_lg{2} = piEXR2ISET(fullfile(DatasetFolder, 'renderings',[thisSName, '_headlights.exr']),'meanluminance',0);
+        if params.headL_wt > 0
             headlight_energy = piReadEXR(fullfile(datasetFolder, [thisSName, '_headlights.exr']), 'data type','radiance');
-            %         weights(2) = params.headL_wt;
         else
-            headlight_energy = zeros(1080, 1920, 31);
+            headlight_energy = zeros(1080, 1920, numBands);
         end
 
-        if p.Results.otherL_wt > 0
-            %         scene_lg{3} = piEXR2ISET(fullfile(DatasetFolder, 'renderings',[thisSName, '_otherlights.exr']),'meanluminance',0);
+        if params.otherL_wt > 0
             otherlight_energy = piReadEXR(fullfile(datasetFolder, [thisSName, '_otherlights.exr']), 'data type','radiance');
-            %         weights(3) = params.otherL_wt;
         else
-            otherlight_energy = zeros(1080, 1920, 31);
+            otherlight_energy = zeros(1080, 1920, numBands);
         end
 
         % Street Lamps
-        if p.Results.streetL_wt > 0
-            %         scene_lg{4} = piEXR2ISET(fullfile(DatasetFolder, 'renderings',[thisSName, '_streetlights.exr']));
+        if params.streetL_wt > 0
             streetlight_energy = piReadEXR(fullfile(datasetFolder, [thisSName, '_streetlights.exr']), 'data type','radiance');
-            %         weights(4) = params.streetL_wt;
         else
-            streetlight_energy = zeros(1080, 1920, 31);
+            streetlight_energy = zeros(1080, 1920, numBands);
         end
 
         % Compose the scene with light groups radiance.
         energy = sky_energy*params.skyL_wt + headlight_energy*params.headL_wt +...
             otherlight_energy*params.otherL_wt + streetlight_energy*params.streetL_wt;
-
-        % Visual wavelengths by default
-        wavelengths = 400:10:700;
 
         % Create an ISETCam scene based on the combination of light sources
         photons  = Energy2Quanta(wavelengths,energy);
@@ -160,7 +169,7 @@ for rr = renderFolders(1):renderFolders(end)
 
         % Keep the lighting params with the scene, making it easier
         % to read them into a db later on
-        scene.metadata.lightingParams = params;
+        scene.metadata.lightingParams = []; %params;
 
         % We also need to load a depth map
         scene.depthMap = piReadEXR(fullfile(datasetFolder, [thisSName, '_otherlights.exr']), 'data type','depth');
@@ -171,34 +180,10 @@ for rr = renderFolders(1):renderFolders(end)
         else
             scene = piAIdenoise(scene, 'quiet', true, 'useNvidia', false);
         end
-        %     save(scenePath,'scene','-mat');
+
+        % Save scene and parameters
         parsave(scenePath, scene, params);
         fprintf('---%d:Saving %s\n',ii,scenePath);
-        %{
-    scene = sceneAdjustLuminance(scene, params.meanluminance);
-    % this example code actually uses a sensor
-    params.sensor.name = 'ar0132atSensorRGB';
-    params.sensor.analoggain = 1;
-
-    if params.flare>1
-        scene = sceneSet(scene, 'distance',0.05);
-        sceneSampleSize = sceneGet(scene,'sample size','m');
-        [oi,pupilmask, psf] = piFlareApply(scene,...
-            'psf sample spacing', sceneSampleSize, ...
-            'numsidesaperture', 5, ...
-            'psfsize', 1920, 'dirtylevel',0);
-        ip = piRadiance2RGB(oi,'etime',1/60,'sensor', params.sensor.name,...
-            'analoggain', params.sensor.analoggain);
-    else
-        oi = piOICreate(scene.data.photons);
-        ip = piRadiance2RGB(oi,'etime',1/60,'sensor', params.sensor.name,...
-            'analoggain', params.sensor.analoggain);
-    end
-    radiance = ipGet(ip,'srgb');
-    imwrite(radiance, sprintf('%s/%s.png',outputFolder, thisSName));
-        %}
-        %     dataset(ii).name = thisSName;
-        %     dataset(ii).distance =  ceil(ii/6)*5+5;
 
     end
 
