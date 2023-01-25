fb6afe39750e22ac7861d2bbf007eb0b2d8b3b5c
David Cardinal <github@cardinalphoto.com>
2023-01-25 12:58:35 -0800

    fix width & height on crop

--- tutorials/t_flareTest.m
+++ tutorials/t_flareTest.m
@@ -3,7 +3,7 @@
 
 % Requires access to scene data
 % Open our database to get a scene (requires isetonline & access to our
-% database. Otherwise get a scene by hand from: 
+% database. Otherwise gets a scene by hand from: 
 %   /acorn/data/iset/isetauto/Ford/SceneISET/nighttime
 
 
@@ -17,7 +17,10 @@ if useDB
     useScene = autoScenes(1); % pick a desired scene from our collection
     
     % Make this more elegant
-    sceneFile = fullfile(useScene.sceneFileFolder,[useScene.sceneID '.mat']);
+    sceneFile = fullfile(useScene.sceneFolder,[useScene.sceneID '.mat']);
+else
+    sceneFile = '/acorn/data/iset/isetauto/Ford/SceneISET/nighttime/1112153442.mat';
+end
 
     ourSceneData = load(sceneFile);
     ourScene = ourSceneData.scene;
@@ -25,6 +28,11 @@ if useDB
     % No actual optics yet
     oi = oiCreate();
     oiScene = oiCompute(ourScene, oi);
+    % need to crop back to scene size
+    % using 'border' is often off by 1, which makes combining data hard
+    sceneSize = size(ourScene.data.photons);
+    % These indices seem to need to be backwards from what I'd think?
+    oiScene = oiCrop(oiScene, [sceneSize(2)/8 sceneSize(1)/8 sceneSize(2)-1 sceneSize(1)-1]); 
     oiFlare = piFlareApply(ourScene); 
 
     oiWindow(oiScene);
@@ -33,11 +41,11 @@ if useDB
     opticsWeight = .96;
     flareWeight = .04;
     oiCombinedData = oiScene.data.photons * opticsWeight + ...
-        flareScene.data.photons * flareWeight;
+        oiFlare.data.photons * flareWeight;
 
     oiCombined = oiScene;
     oiCombined.data.photons = oiCombinedData;
 
     oiWindow(oiCombined);
-    
+
 end
