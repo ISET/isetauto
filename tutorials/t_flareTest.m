0b6aebd079077e4d2ab9461c4a45febb7386c601
David Cardinal <github@cardinalphoto.com>
2023-01-25 13:08:04 -0800

    cleanup comments

--- tutorials/t_flareTest.m
+++ tutorials/t_flareTest.m
@@ -6,7 +6,12 @@
 % database. Otherwise gets a scene by hand from: 
 %   /acorn/data/iset/isetauto/Ford/SceneISET/nighttime
 
+% Set relative weights for light that travels per the optics model
+% and light that gets to the sensor some other way
+opticsWeight = .96;
+flareWeight = .04;
 
+% Set to false if you just want to load a scene from a file directly
 useDB = true;
 
 if useDB
@@ -31,15 +36,20 @@ end
     % need to crop back to scene size
     % using 'border' is often off by 1, which makes combining data hard
     sceneSize = size(ourScene.data.photons);
+    
     % These indices seem to need to be backwards from what I'd think?
     oiScene = oiCrop(oiScene, [sceneSize(2)/8 sceneSize(1)/8 sceneSize(2)-1 sceneSize(1)-1]); 
+    
+    % N.B. We don't crop the depth map (yet), but we also don't use it
+    % (yet)
+    
+    % now calculate the irradiance from flare
+    % we can/should use parameters that mimic our choice of lens
     oiFlare = piFlareApply(ourScene); 
 
     oiWindow(oiScene);
     oiWindow(oiFlare);
 
-    opticsWeight = .96;
-    flareWeight = .04;
     oiCombinedData = oiScene.data.photons * opticsWeight + ...
         oiFlare.data.photons * flareWeight;
 
