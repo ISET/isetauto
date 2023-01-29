8a26b5c527a6c4f6567ff0a7c9e704466cf21e95
Brian Wandell <wandell@stanford.edu>
2023-01-29 14:37:48 -0800

    Update s_MTF50vsOD50.m

--- papers/EI2023/s_MTF50vsOD50.m
+++ papers/EI2023/s_MTF50vsOD50.m
@@ -1,6 +1,14 @@
 %% mtf vs od50
+%
+%  Computed analysis by Zhenyi of the relationship between MTF50 and OD50
+%
+% See also
+%   s_ei*
+
+%% Values
 mtf = [55 55 55 55 75 75 75 75 100 100 100 150 150];
 
+% Separated by night and day
 od50_night = [40.3509   43.3761   30.2885   35.4167   34.9673   24.2857   22.6000   23.9130   27.8662   13.2911   11.7021   19.5087   18.9306];
 
 od50_day = [172.6316  241.0256  128.7037  137.7586  137.3967  103.6545  105.3872  119.5440  129.7710   88.0155  101.7188   96.3750   90.7834];
@@ -8,29 +16,64 @@ od50_day = [172.6316  241.0256  128.7037  137.7586  137.3967  103.6545  105.3872
 od = od50_day;
 od = flip(od);
 
-ieNewGraphWin;
+%%
+ieNewGraphWin([],'tall');
+symSize = 600;
+lWidth = 1.5;
+scatter(mtf(1), od(1), symSize, 'o', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(2), od(2), symSize,'o', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(3), od(3), symSize,'o', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(4), od(4), symSize,'o', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(5), od(5), symSize,'diamond', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(6), od(6), symSize,'diamond', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(7), od(7), symSize,'diamond', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(8), od(8), symSize,'diamond', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(9), od(9), symSize,'^', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(10), od(10), symSize,'^', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(11), od(11), symSize,'^', 'filled','LineWidth',lWidth); hold on
+scatter(mtf(12), od(12), symSize,"hexagram", 'filled','LineWidth',lWidth); hold on
+scatter(mtf(13), od(13), symSize,"hexagram", 'filled','LineWidth',lWidth); 
 
-scatter(mtf(1), od(1), 600, 'o', 'filled','LineWidth',1.5); hold on
-scatter(mtf(2), od(2), 600,'o', 'filled','LineWidth',1.5); hold on
-scatter(mtf(3), od(3), 600,'o', 'filled','LineWidth',1.5); hold on
-scatter(mtf(4), od(4), 600,'o', 'filled','LineWidth',1.5); hold on
-scatter(mtf(5), od(5), 600,'diamond', 'filled','LineWidth',1.5); hold on
-scatter(mtf(6), od(6), 600,'diamond', 'filled','LineWidth',1.5); hold on
-scatter(mtf(7), od(7), 600,'diamond', 'filled','LineWidth',1.5); hold on
-scatter(mtf(8), od(8), 600,'diamond', 'filled','LineWidth',1.5); hold on
-scatter(mtf(9), od(9), 600,'^', 'filled','LineWidth',1.5); hold on
-scatter(mtf(10), od(10), 600,'^', 'filled','LineWidth',1.5); hold on
-scatter(mtf(11), od(11), 600,'^', 'filled','LineWidth',1.5); hold on
-scatter(mtf(12), od(12), 600,"hexagram", 'filled','LineWidth',1.5); hold on
-scatter(mtf(13), od(13), 600,"hexagram", 'filled','LineWidth',1.5); 
-
-% end
-xlabel('MTF50 (c/mm)');
-ylabel('OD50 (meters)'); 
+% xlabel('MTF50 (c/mm)');
+% ylabel('OD50 (meters)'); 
 % ylim([50,300]); xlim([50, 200]);
 grid on
 set(gca,'fontsize', 28); 
 
+
+
+%%
+
+od = od50_night;
+od = flip(od);
+
+hold on
+% ieNewGraphWin;
+lWidth = 2;
+
+scatter(mtf(1), od(1), symSize, 'o', 'LineWidth',lWidth); hold on
+scatter(mtf(2), od(2), symSize,'o', 'LineWidth',lWidth); hold on
+scatter(mtf(3), od(3), symSize,'o', 'LineWidth',lWidth); hold on
+scatter(mtf(4), od(4), symSize,'o', 'LineWidth',lWidth); hold on
+scatter(mtf(5), od(5), symSize,'diamond', 'LineWidth',lWidth); hold on
+scatter(mtf(6), od(6), symSize,'diamond', 'LineWidth',lWidth); hold on
+scatter(mtf(7), od(7), symSize,'diamond', 'LineWidth',lWidth); hold on
+scatter(mtf(8), od(8), symSize,'diamond', 'LineWidth',lWidth); hold on
+scatter(mtf(9), od(9), symSize,'^', 'LineWidth',lWidth); hold on
+scatter(mtf(10), od(10), symSize,'^', 'LineWidth',lWidth); hold on
+scatter(mtf(11), od(11), symSize,'^', 'LineWidth',lWidth); hold on
+scatter(mtf(12), od(12), symSize,"hexagram", 'LineWidth',lWidth); hold on
+scatter(mtf(13), od(13), symSize,"hexagram", 'LineWidth',lWidth); 
+
+% xlabel('MTF {50} (cycles/mm)');
+% ylabel('OD {50} (m)'); 
+set(gca,'fontsize', 28); 
+grid on
+
+set(gca,'xlim',[50 200])
+set(gcf,'Position',[0.0070 0.4486 0.2800 0.4564]);
+%%
+%{
 lgnd{1} = 'ps4.2um-f#3.1';
 lgnd{2} = 'ps3.5um-f#6.4';
 lgnd{3} = 'ps2.8um-f#9.1';
@@ -45,3 +88,7 @@ lgnd{11} = 'ps1um-f#6.1';
 lgnd{12} = 'ps1.4um-f#1.9';
 lgnd{13} = 'ps1um-f#3.3';
 [lgd] = legend(lgnd,'Location','northwest','NumColumns',4);lgd.FontSize = 20;
+%}
+
+%%
+
