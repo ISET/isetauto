5abf633a592e7c8b6845b36a40eba32e16610213
David Cardinal <github@cardinalphoto.com>
2023-01-29 09:35:39 -0800

    Add params for a daytime scene

--- tutorials/t_makeScenesFromRenders.m
+++ tutorials/t_makeScenesFromRenders.m
@@ -13,7 +13,11 @@
 % Choose folder of rendered files to turn into scenes
 % based on our project and scenario
 project = 'Ford';
-scenarioName = sprintf('nighttime_%s',datetime('now','Format','yy-MM-dd-HH-mm'));
+% Original was night time
+%scenarioName = sprintf('nighttime_%s',datetime('now','Format','yy-MM-dd-HH-mm'));
+
+% let's try one with more day-like params
+scenarioName = sprintf('daytime_%s',datetime('now','Format','yy-MM-dd-HH-mm'));
 
 % Can pass one or more folders to render
 % And an output folder name if desired
@@ -23,9 +27,17 @@ renderFolder = fullfile(iaFileDataRoot('local', false), project, 'SceneEXRs');
 outputFolder = fullfile(iaFileDataRoot('local', false), project, 'SceneISET', scenarioName); 
 
 maxImages = 4; % set for debugging, otherwise < 0 means all
+
+% Original nighttime params
+%useArgs = {'scenarioname', scenarioName, ...
+%    'skyl_wt', 10, 'meanluminance', 5, 'headl_wt', 1, ...
+%    'otherl_wt', 1, 'streetl_wt', .5, 'maxImages', maxImages, ...
+%    'outputFolder', outputFolder};
+
+% Bump daylight, mean luminance, remove street & headlights
 useArgs = {'scenarioname', scenarioName, ...
-    'skyl_wt', 10, 'meanluminance', 5, 'headl_wt', 1, ...
-    'otherl_wt', 1, 'streetl_wt', .5, 'maxImages', maxImages, ...
+    'skyl_wt', 20, 'meanluminance', 500, 'headl_wt', 0, ...
+    'otherl_wt', 1, 'streetl_wt', 0, 'maxImages', maxImages, ...
     'outputFolder', outputFolder};
 
 % Now execute the conversion. This can take a long time.
