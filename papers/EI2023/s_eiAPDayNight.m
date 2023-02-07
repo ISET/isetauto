32edf25a1bcfa29629eac0199386024c05eb48af
Brian Wandell <wandell@stanford.edu>
2023-02-06 16:39:53 -0800

    Plot graphs for paper

--- papers/EI2023/s_eiAPDayNight.m
+++ papers/EI2023/s_eiAPDayNight.m
@@ -4,6 +4,9 @@
 % There are thirteen different cameras
 %
 %%
+ieInit;
+
+%%
 symColors = [...
     0.2 0.2 0.8; % dark blue
     0 0.6 0.8;   % cyan
@@ -36,6 +39,33 @@ ap_day = [0.889	0.873	0.876	0.87	0.891	0.87	0.859	0.861	0.847	0.849	0.842	0.839	
 0.543	0.571	0.362	0.429	0.439	0.221	0.235	0.313	0.394	0.0997	0.191	0.263	0.225
 0.448	0.532	0.277	0.31	0.357	0.219	0.17	0.188	0.29	0.0718	0.108	0.182	0.189];
 
+
+%% Day
+ieNewGraphWin;
+for ii = 1:13
+    plot(distance, ap_day(:,14-ii),'-','LineWidth',lWidth,'LineStyle','-','Color',[0.5 0.5 0.5]);  % Lines
+    hold on
+    scatter(distance, ap_day(:,14-ii),symSize,symColors(ii,:),sym{ii},'filled','LineWidth',lWidth);
+    hold on;
+end
+set(gca,'fontsize', 28); 
+grid on
+set(gca,'ylim',[0 1]);
+
+
+%% Day
+ieNewGraphWin;
+symSize = 400;
+for ii = [12 9 5 1]
+    plot(distance, ap_day(:,14-ii),'-','LineWidth',lWidth,'LineStyle','-','Color',[0.5 0.5 0.5]);  % Lines
+    hold on
+    scatter(distance, ap_day(:,14-ii),symSize,symColors(ii,:),sym{ii},'filled','LineWidth',lWidth);
+    hold on;
+end
+set(gca,'fontsize', 28); 
+grid on
+set(gca,'ylim',[0 1]);
+
 %% night
 ap_night = [0.57	0.586	0.522	0.55	0.561	0.496	0.488	0.493	0.518	0.426	0.425	0.462	0.458
 0.456	0.469	0.418	0.43	0.408	0.356	0.363	0.332	0.361	0.268	0.284	0.289	0.285
@@ -43,18 +73,6 @@ ap_night = [0.57	0.586	0.522	0.55	0.561	0.496	0.488	0.493	0.518	0.426	0.425	0.46
 0.33	0.357	0.276	0.302	0.304	0.186	0.189	0.243	0.245	0.127	0.157	0.176	0.146
 0.192	0.212	0.108	0.124	0.142	0.041	0.0589	0.0751	0.0994	0.0148	0.0228	0.0278	0.0339
 0.0936	0.14	0.0339	0.0516	0.0721	0.016	0.0182	0.0179	0.1	0.0089	0.0142	0.0279	0.0092];
-
-%% Day
-ieNewGraphWin;
-for ii = 13:-1:1
-    plot(distance, ap_day(:,ii),'-','LineWidth',lWidth,'LineStyle','-','Color',[0.5 0.5 0.5]);  % Lines
-    hold on
-    scatter(distance, ap_day(:,ii),symSize,symColors(ii,:),sym{ii},'filled','LineWidth',lWidth);
-    hold on;
-end
-set(gca,'fontsize', 28); 
-grid on
-set(gca,'ylim',[0 1]);
 
 %% Night
 ieNewGraphWin;
