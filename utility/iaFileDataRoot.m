c8e9cb69ecb34dc76311b7eac95095463b81ec82
David Cardinal <github@cardinalphoto.com>
2023-02-23 18:03:48 -0800

    check in routine to find the root for file data

---
+++ utility/iaFileDataRoot.m
@@ -0,0 +1,29 @@
+function dataRoot = iaFileDataRoot(varargin)
+%IADATAROOT Get root of our Data Files
+%   This is where we look for data that is too large to fit in our
+%   repo. Typically it will be on a network file server, although
+%   for performance cloning it and setting your pref to use the cloned
+%   version is certainly possible.
+
+p = inputParser();
+addParameter(p, 'local', false); % Use a local cache for performance
+
+% convert our args to ieStandard and parse
+varargin = ieParamFormat(varargin);
+p.parse(varargin{:});
+
+dataRoot = getpref('isetauto', 'filedataroot', '');
+
+% These are a bit of a guess, but based on acorn fs
+if isempty(dataRoot)
+    if ispc
+        if p.Results.local == true
+            dataRoot = 'v:\data\iset\isetauto';
+        else
+            dataRoot = 'y:\data\iset\isetauto';
+        end
+    else
+        dataRoot = '/acorn/data/iset/isetauto';
+    end
+end
+
