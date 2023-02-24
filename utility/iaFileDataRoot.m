c19e74a63559ff5f4f5815e5e483c79c971cc717
David Cardinal <github@cardinalphoto.com>
2023-02-24 15:06:48 -0800

    add support for pbrt assets

--- utility/iaFileDataRoot.m
+++ utility/iaFileDataRoot.m
@@ -8,23 +8,28 @@ function dataRoot = iaFileDataRoot(varargin)
 
 p = inputParser();
 addParameter(p, 'local', false); % Use a local cache for performance
+addParameter(p, 'type', 'filedata');
 
 % convert our args to ieStandard and parse
 varargin = ieParamFormat(varargin);
 p.parse(varargin{:});
 
-dataRoot = getpref('isetauto', 'filedataroot', '');
+switch (p.Results.type)
+    case 'filedata'
+        dataRoot = getpref('isetauto', 'filedataroot', '');
 
-% These are a bit of a guess, but based on acorn fs
-if isempty(dataRoot)
-    if ispc
-        if p.Results.local == true
-            dataRoot = 'v:\data\iset\isetauto';
-        else
-            dataRoot = 'y:\data\iset\isetauto';
+        % These are a bit of a guess, but based on acorn fs
+        if isempty(dataRoot)
+            if ispc
+                if p.Results.local == true
+                    dataRoot = 'v:\data\iset\isetauto';
+                else
+                    dataRoot = 'y:\data\iset\isetauto';
+                end
+            else
+                dataRoot = '/acorn/data/iset/isetauto';
+            end
         end
-    else
-        dataRoot = '/acorn/data/iset/isetauto';
-    end
+    case 'PBRT_assets'
+        dataRoot = '/acorn/data/iset/isetauto/PBRT_assets';
 end
-
