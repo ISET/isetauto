5dad9b30aaf68fff69d2d3045610618a2b475d28
David Cardinal <github@cardinalphoto.com>
2023-02-25 08:50:38 -0800

    more explanatory comments

--- scripts/s_recipeRender.m
+++ scripts/s_recipeRender.m
@@ -72,9 +72,9 @@ sceneWindow(rightGrillScene);
 % The .mat file includes an @recipe struct called thisR
 ourRecipe = recipeWrapper.thisR;
 
+%% Fix up our recipe in lieu of piRead()
 % Since we aren't/can't use piRead() the normal path fixes for input and
-% output have not been applied (leaving the same as the original authors)
-
+% output have not been applied, so we need to do that manually...
 
 % So the next idea is to set the inputfile to the original base recipe
 % file (in the Ford case that is always 1 of 12 road recipes).
@@ -97,17 +97,19 @@ ourRecipe.outputFile = fullfile(piDirGet('local'), sceneID, [sceneID '.pbrt']);
 
 piWrite(ourRecipe);
 
+%% Try to get the written out recipe to render
 % Our first issue is that a lot of the textures get copied to the recipe
 % folder, not the textures sub-folder (maybe the ones we found in our
 % path?)
 % Short term I just brute move them in /local after running piWrite()
 
 
-% Then we find the same issue with mixing textures, so we copy all of 
+% Then we find the same issue with missing textures, so we copy all of 
 % those into our scene in local by hand
 
 % Then we get to trickier stuff like this:
 
+%% What we'd like to have work:
 
 scene = piRender(ourRecipe);
 
