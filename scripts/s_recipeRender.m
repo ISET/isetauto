94b21af57e294d66136a7f08f8c7a4a341ea81f7
David Cardinal <github@cardinalphoto.com>
2023-02-25 05:53:12 -0800

    Document the process of trying to render an Auto scene

--- scripts/s_recipeRender.m
+++ scripts/s_recipeRender.m
@@ -72,6 +72,42 @@ sceneWindow(rightGrillScene);
 % The .mat file includes an @recipe struct called thisR
 ourRecipe = recipeWrapper.thisR;
 
-piWRS(ourRecipe);
+% Since we aren't/can't use piRead() the normal path fixes for input and
+% output have not been applied (leaving the same as the original authors)
+
+
+% So the next idea is to set the inputfile to the original base recipe
+% file (in the Ford case that is always 1 of 12 road recipes).
+% Here is an example of what is coded in the @recipe:
+% '/Volumes/SSDZhenyi/Ford Project/PBRT_assets/road/road_012/road_012/road_012.pbrt'
+
+[rPath, rName, rExtension] = fileparts(ourRecipe.inputFile);
+
+% Hack for the road recipe folder structure
+assetFolder = iaFileDataRoot('type','PBRT_assets');
+recipePBRT = fullfile(assetFolder, 'road', rName, rName, [rName rExtension]);
+ourRecipe.inputFile = recipePBRT;
+
+ourRecipe.outputFile = fullfile(piDirGet('local'), sceneID, [sceneID '.pbrt']);
+
+piWrite(ourRecipe);
+
+% Our first issue is that a lot of the textures get copied to the recipe
+% folder, not the textures sub-folder (maybe the ones we found in our
+% path?)
+% Short term I just brute move them in /local after running piWrite()
+
+
+% Then we find the same issue with mixing textures, so we copy all of 
+% those into our scene in local by hand
+
+% Then we get to trickier stuff like this:
+
+
+scene = piRender(ourRecipe);
+
+sceneWindow(scene);
+
+
 
 
