de9e7410e6d81e5f66e713bfa81ae90c0cb137f4
David Cardinal <github@cardinalphoto.com>
2023-04-14 09:25:05 -0700

    better comments, clean up merge

--- scripts/s_recipeRender.m
+++ scripts/s_recipeRender.m
@@ -1,25 +1,33 @@
 % Test script to see if we can read and render the Ford recipes
 % that we have on Acorn:
 
-% Pick an arbitrary scene/recipe
+% Pick an arbitrary scene/recipe -- default is the one that we've added
+% to data/scenes in the ISETAuto repo
 sceneID = '1112154540';
 
 %% Read in the @recipe object
-% We can't read back the piWrite() version of a recipe, so
-% we need to read the @recipe object from a .mat file
+% We can't read back the piWrite() version of our Auto recipes, so
+% we need to read the @recipe object from the initial .mat file
 
-% NOTE: iaFileDataRoot needs to point to the root of where
-%       you have access to the scenes and assets. It tries
-%       to guess, but you can also:
+% NOTE: For general use iaFileDataRoot needs to point to the root of where
+%       you have access to the scenes (and assets if you don't use remote
+%       rendering. It tries to guess, but you can also:
 %           setpref('isetauto', 'filedataroot', '<location>')
-%
-recipeFolder = fullfile(iaFileDataRoot(), 'Ford','SceneRecipes');
+%       or scenes need to be in your Matlab path.
+
+% We have checked one sample scene into the ISETAuto repo, so you can
+% run this script as a tutorial using it without any other setup
+
+% Our @recipe objects are stored in .mat files by sceneID
 recipeFileName = [sceneID '.mat'];
 
-% We may have checked this in to the repo
+% We may have checked this in to the repo or put it in our path
 if which(recipeFileName)
     recipeFile = recipeFileName;
 else
+    % if not, look for it where our ISETAuto data is
+    % This could also be an isetdb() lookup 
+    recipeFolder = fullfile(iaFileDataRoot(), 'Ford','SceneRecipes');
     recipeFile = fullfile(recipeFolder,recipeFileName);
 end
 
@@ -29,15 +37,25 @@ recipeWrapper = load(recipeFile);
 initialRecipe = recipeWrapper.thisR;
 
 %% Fix up our recipe in lieu of piRead()
+
 % Since we aren't/can't use piRead() the normal path fixes for input and
 % output have not been applied, so we need to do that manually...
 [rPath, rName, rExtension] = fileparts(initialRecipe.inputFile);
 
 % Hack for the road recipe folder structure
 assetFolder = iaFileDataRoot('type','PBRT_assets');
+
+% ?? Does the inputfile / original pbrt matter when we do piWrite?
 recipePBRT = fullfile(assetFolder, 'road', rName, rName, [rName rExtension]);
 
 % These fixups are normally done by piRead()
+% But since we are getting @recipe directly from a .mat file we need
+% to handle updating the inputfile and outputfile ourselves.
+% ALSO: Auto recipes currently all use one of just a few Road scenes
+%       as their inputfile, even though the thousands of scenes are
+%       unique.
+
+% Initial Recipe is prior to any edits we make
 initialRecipe.inputFile = recipePBRT;
 initialRecipe.outputFile = fullfile(piDirGet('local'), sceneID, [sceneID '-initial.pbrt']);
 
@@ -51,77 +69,24 @@ recipeSet(initialRecipe,'filmresolution', [480 270])
 rightGrillRecipe = piRecipeCopy(initialRecipe);
 rightGrillRecipe = piCameraTranslate(rightGrillRecipe, 'x shift', -.5, ...
     'y shift', -.5, 'z shift', 2);
+
+% Give it its own output pbrt filename
 rightGrillRecipe.outputFile = fullfile(piDirGet('local'), [sceneID '-rgrill'], [sceneID '-rgrill.pbrt']);
 
 % Write our recipe to a file tree, so that pbrt can process it
-% setpref('docker','remoteResources', false);
 piWrite(initialRecipe);
 piWrite(rightGrillRecipe);
 
-initialScene = piRender(initialRecipe);
+% Render our initial scene using the resources already on our server
+initialScene = piRender(initialRecipe, 'remoteResources',true);
+
 % Show the result
 sceneWindow(initialScene);
 
-rightGrillScene = piRender(rightGrillRecipe);
+% Now render and show our scene with the camera on the right side of the grill
+rightGrillScene = piRender(rightGrillRecipe, 'remoteResources', true);
 sceneWindow(rightGrillScene);
 
-
-
-
-
-% The .mat file includes an @recipe struct called thisR
-ourRecipe = recipeWrapper.thisR;
-
-%% Fix up our recipe in lieu of piRead()
-% Since we aren't/can't use piRead() the normal path fixes for input and
-% output have not been applied, so we need to do that manually...
-
-% So the next idea is to set the inputfile to the original base recipe
-% file (in the Ford case that is always 1 of 12 road recipes).
-% Here is an example of what is coded in the @recipe:
-% '/Volumes/SSDZhenyi/Ford Project/PBRT_assets/road/road_012/road_012/road_012.pbrt'
-
-[rPath, rName, rExtension] = fileparts(ourRecipe.inputFile);
-
+%% Other experiments
 % Experiment with moving the camera above the car
-ourRecipe.lookAt.from(2) = ourRecipe.lookAt.from(2) + 3;
-
-
-% Hack for the road recipe folder structure
-assetFolder = iaFileDataRoot('type','PBRT_assets');
-recipePBRT = fullfile(assetFolder, 'road', rName, rName, [rName rExtension]);
-
-% Failed Experiment: What if we use the version created by piWrite as our
-% pbrt file -- Oops, piWriteCopy then pulls everything ...
-%recipePBRT = fullfile(recipeFolder, [sceneID '.pbrt']);
-
-% These fixups are normally done by piRead()
-ourRecipe.inputFile = recipePBRT; 
-ourRecipe.outputFile = fullfile(piDirGet('local'), sceneID, [sceneID '.pbrt']);
-
-piWrite(ourRecipe);
-
-%% Try to get the written out recipe to render
-% Our first issue is that a lot of the textures get copied to the recipe
-% folder, not the textures sub-folder (maybe the ones we found in our
-% path?)
-% Short term I just brute move them in /local after running piWrite()
-
-
-% Then we find the same issue with missing textures, so we copy all of 
-% those into our scene in local by hand
-
-% Then we get to trickier stuff like this:
-
-%% What we'd like to have work:
-
-scene = piRender(ourRecipe);
-
-% Show the result
-sceneWindow(scene);
-
-
-
-
-
-
+%ourRecipe.lookAt.from(2) = ourRecipe.lookAt.from(2) + 3;
