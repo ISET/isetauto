d05bf9ad6d5366b81e62d211616cc8cdfe8a35db
David Cardinal <github@cardinalphoto.com>
2023-04-15 13:55:26 -0700

    update comments

--- scripts/s_recipeRender.m
+++ scripts/s_recipeRender.m
@@ -5,6 +5,10 @@
 %  It then demonstrates how to move the camera position to the right
 %  grille of the vehicle and re-render the scene.
 %
+
+% NOTE: Our Auto scenes consume around 12GB of GPU memory when
+%       rendered on a GPU
+
 % NOTE: These are HDR scenes, so they will initially appear black
 %       in the scene window until to change the render to HDR
 
@@ -15,8 +19,8 @@
 sceneID = '1112154540';
 
 %% Read in the @recipe object
-% We can't read back the piWrite() version of our Auto recipes, so
-% we need to read the @recipe object from the initial .mat file
+% We can't read back the piWrite()->.pbrt version of our Auto recipes, so
+% we need to read the initial @recipe object from the the .mat file
 
 % NOTE: For general use iaFileDataRoot needs to point to the root of where
 %       you have access to the scenes (and assets if you don't use remote
@@ -40,56 +44,49 @@ else
     recipeFile = fullfile(recipeFolder,recipeFileName);
 end
 
+% The .mat file contains a thisR @recipe inside
 recipeWrapper = load(recipeFile);
-
-% The .mat file includes an @recipe object called thisR
 initialRecipe = recipeWrapper.thisR;
 
 %% Fix up our recipe in lieu of piRead()
+% These fixups are normally done by piRead()
+% But since we are getting @recipe directly from a .mat file we need
+% to handle updating the inputfile and outputfile ourselves.
 
-% Since we aren't/can't use piRead() the normal path fixes for input and
-% output have not been applied, so we need to do that manually...
 [rPath, rName, rExtension] = fileparts(initialRecipe.inputFile);
 
+%% Fix the inputfile path to the road recipe used -- may not be needed?
+% ALSO: Auto recipes currently all use one of just a few Road scenes
+%       as their inputfile, even though the thousands of scenes are
+%       unique.
 % Hack for the road recipe folder structure
 assetFolder = iaFileDataRoot('type','PBRT_assets');
 
 % ?? Does the inputfile / original pbrt matter when we do piWrite?
 recipePBRT = fullfile(assetFolder, 'road', rName, rName, [rName rExtension]);
-
-% These fixups are normally done by piRead()
-% But since we are getting @recipe directly from a .mat file we need
-% to handle updating the inputfile and outputfile ourselves.
-% ALSO: Auto recipes currently all use one of just a few Road scenes
-%       as their inputfile, even though the thousands of scenes are
-%       unique.
-
-% Initial Recipe is prior to any edits we make
 initialRecipe.inputFile = recipePBRT;
+
+% This Recipe is prior to any edits we make
+% So we'll call it <sceneID>-initial
 initialRecipe.outputFile = fullfile(piDirGet('local'), sceneID, [sceneID '-initial.pbrt']);
 
 % Scale down the scene resolution to make it faster to render
 % (Auto scenes are 1080p native)
 recipeSet(initialRecipe,'filmresolution', [480 270])
 
+% first make a copy before we make changes 
+rightGrillRecipe = piRecipeCopy(initialRecipe);
+
 % Move the camera to the front-right of the car
 % initial position is behind windshield
-
-% I'm pretty sure this was right when I first rendered the scene:
-% x is vertical, y is left, and z is forward
-
-% Now it seems like
 % x is vertical, y is right, and z is backward
-
-% recipe copy might not be working
-rightGrillRecipe = piRecipeCopy(initialRecipe);
 rightGrillRecipe = piCameraTranslate(rightGrillRecipe, 'x shift', -.5, ...
     'y shift', 1, 'z shift', -1.5);
 
-% Give it its own output pbrt filename
+% Give our modified recipe its own output pbrt filename
 rightGrillRecipe.outputFile = fullfile(piDirGet('local'), [sceneID '-rgrill'], [sceneID '-rgrill.pbrt']);
 
-% Write our recipe to a file tree, so that pbrt can process it
+% Write our recipes to file trees in 'local', so that pbrt can process it
 piWrite(initialRecipe);
 piWrite(rightGrillRecipe);
 
@@ -102,7 +99,3 @@ sceneWindow(initialScene);
 % Now render and show our scene with the camera on the right side of the grill
 rightGrillScene = piRender(rightGrillRecipe, 'remoteResources', true);
 sceneWindow(rightGrillScene);
-
-%% Other experiments
-% Experiment with moving the camera above the car
-%ourRecipe.lookAt.from(2) = ourRecipe.lookAt.from(2) + 3;
