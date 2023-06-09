3a092cee46982a76f27e0893bc22130549d8f620
David Cardinal <github@cardinalphoto.com>
2023-06-09 09:40:16 -0700

    initial work on calling subclass

--- @ia_drivingScenario/ia_drivingScenario.m
+++ @ia_drivingScenario/ia_drivingScenario.m
@@ -6,6 +6,16 @@ classdef ia_drivingScenario < drivingScenario
     end
     
     methods
+        function ds = ia_drivingScenario(varargin)
+
+            % Let the Matlab driving scenario set things up first
+            % ds now contains a "blank slate" scenario
+            try
+                %parseInputs(ds, varargin{:});
+            catch ME
+                throwAsCaller(ME);
+            end
+        end
     end
 end
 
