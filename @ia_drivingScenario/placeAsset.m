979506bc851c9963d8e4deedd96498f112a692cb
David Cardinal <github@cardinalphoto.com>
2023-06-10 08:24:18 -0700

    stub for asset placement using SDS

---
+++ @ia_drivingScenario/placeAsset.m
@@ -0,0 +1,42 @@
+function placeAsset(scenario, assetName, position, rotation)
+%IAPLACEASSET Quick way to manually place assets in auto scenes
+%   Caveat: There might already be some code for this in ISET3d
+%           but I couldn't find it
+%   Related: It might actually be general enough to put in ISET3d, but
+%            wanted to get the ball rolling for Auto stuff
+%
+%   D.Cardinal, Stanford, May, 2023
+%
+
+%% For Matlab scenes, we get a scenario object
+
+assetBranchName = [assetName '_B'];
+assetFileName = [assetName '.pbrt'];
+
+assetRecipe = piRead(assetFileName);
+
+% We want to translate to a location relative to the camera
+% Our assumption is that the asset is at 0,0,0 in the asset recipe
+%% This needs to change
+cameraLocation = scenario.recipe.lookAt.from;
+
+% Adjust for x-axis being towards the car in Ford scenes
+
+%% For vehicles from Matlab's DSD we need to do this differently
+
+% old way
+if ~isempty(position)
+    % position is where we want to be relative to car in x and y
+    % and relative to ground in z
+    assetTranslation(1) = cameraLocation(1) - position(1);
+    assetTranslation(2) = cameraLocation(2) + position(2);
+    assetTranslation(3) = position(3); 
+    assetBranch = piAssetTranslate(assetRecipe,assetBranchName, assetTranslation);
+end
+if ~isempty(rotation)
+    assetBranch = piAssetRotate(assetRecipe,assetBranchName,rotation);
+end
+
+scenario.roadRecipe = piRecipeMerge(scenario.roadRecipe, assetRecipe);
+
+end
