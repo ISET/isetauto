dd0be108f14fcf4a25fa0dc68cd87da17232080b
David Cardinal <github@cardinalphoto.com>
2023-06-26 09:09:01 -0700

    Initial experiment with Matlab experiments

---
+++ experiments/e_paeb_initial/e_paeb_run.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = e_paeb_run(inputArg1,inputArg2)
+%E_PAEB_RUN Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
