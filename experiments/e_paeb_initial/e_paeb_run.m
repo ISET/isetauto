472ebec8c69dff3c3db405dc6760a9d41fd5cf46
David Cardinal <github@cardinalphoto.com>
2023-06-29 08:12:53 -0700

    update comments

--- experiments/e_paeb_initial/e_paeb_run.m
+++ experiments/e_paeb_initial/e_paeb_run.m
@@ -1,8 +1,12 @@
 function  e_paeb_run(params)
 %E_PAEB_RUN run pedestrian aeb experiment in matlab experiment manager
 
+    % Pass params in using static methods of our scenario class, since
+    % we don't have an object of the class yet
     ia_drivingScenario.initialSpeed(params.initialSpeed);
-    ia_drivingScenario.inExperiment(true); % some code won't run right in an experiment
+    
+    % some code won't run right in an experiment
+    ia_drivingScenario.inExperiment(true); 
 
     % this is the driving scenario function exported from the DSD
     % modified to invoke our version of driving scenario.
