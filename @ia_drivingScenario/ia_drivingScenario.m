522cd21da6150d1acc2b4d92fbce2e359d0feda3
David Cardinal <github@cardinalphoto.com>
2023-09-21 15:59:11 -0700

    change to quick

--- @ia_drivingScenario/ia_drivingScenario.m
+++ @ia_drivingScenario/ia_drivingScenario.m
@@ -34,8 +34,8 @@ classdef ia_drivingScenario < drivingScenario
         % (e.g. real frame rates, sceneResolution > cameraResolution
         %       lots of rays, and no de-noising)
 
-        stepTime = .06; % time per image frame/step
-        scenarioQuality = 'HD'; 
+        stepTime = 1; % time per image frame/step
+        scenarioQuality = 'quick'; 
 
         deNoise = 'exr_albedo'; % can use 'exr_radiance', 'exr_albedo', 'scene', or ''
         % For debugging raise the camera and look down
